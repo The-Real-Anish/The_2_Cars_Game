186c36d86d826d255ca8a72a7aff0bcf812f64ca
The-Real-Anish <anishkul38@gmail.com>
2022-10-19 02:02:33 +0530

    Successfully translated the cars and allowed multi-car movements while restricting cars to only the lanes

---
+++ app/src/main/java/com/example/cppapp/Dumpyard.kt
@@ -0,0 +1,6 @@
+package com.example.cppapp
+
+class Dumpyard {
+
+
+}
--- app/src/main/java/com/example/cppapp/MainActivity.kt
+++ app/src/main/java/com/example/cppapp/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.cppapp
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnClickListener
+import android.view.View.OnTouchListener
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
 import android.widget.ImageView
+import androidx.core.view.marginLeft
+import androidx.core.view.marginStart
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("RestrictedApi")
+    @SuppressLint("RestrictedApi", "ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        val blue_car: ImageView? = findViewById(R.id.car_1)
-
-        val red_car: ImageView? = findViewById(R.id.car_2)
-
-        val screen: ImageView? = findViewById(R.id.game_screen)
 
         //var blue_rightturn: Boolean = true
         //var red_rightturn: Boolean = false
@@ -24,26 +28,134 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //bringing the game screen and cars into the play
+        val blueCar: ImageView = findViewById(R.id.car_1)
 
-        screen?.setOnClickListener {
+        val redCar: ImageView = findViewById(R.id.car_2)
 
-            blue_car!!.animate().apply {
-                duration = 500
-                rotationBy(30f)
-                //translationYBy(-50f)
-            }.start()
+        val screen: ImageView = findViewById(R.id.game_screen)
 
-            //blue_rightturn = !blue_rightturn
+        val centralLine: View = findViewById(R.id.line_2)
 
-            //red_car.animate().apply {
-            //    duration = 500
-            //    rotationBy(-30f)
-            //    translationXBy(-102f)
-            //}.start()
+        val distance: Float = centralLine.marginStart.toFloat()
 
-            //red_rightturn = !red_rightturn
+        var blueGoRight = true
+        var redGoRight = false
+        var animationEnded = true
 
+        val blueEvent: Array<MotionEvent> = emptyArray()
+        val redEvent: Array<MotionEvent> = emptyArray()
+        var is_blue_event = false
+        var is_red_event = false
+        var prevTime: Long = 0
+        var bluePrevTime: Long = 0
+        var redPrevTime: Long = 0
+
+        //val blue_moveright = ObjectAnimator.ofFloat(blue_car, View.TRANSLATION_X,
+        //                                            blue_car.translationX, blue_car.translationX + 102.5f)
+        //blue_moveright.duration = 500
+        //blue_moveright.interpolator = LinearInterpolator()
+
+        //blue_moveright.start()
+
+        //fun setOnClickListenerWithPoint(action: (Point) -> Int) {
+
+        //}
+
+        //screen.setOnTouchListener()
+
+        /*screen.setOnClickListener { view ->
+
+            var touchedX = null
+            Log.d("Abscissa", touchedX.toString())
+            Log.d("Distance of line", distance.toString())
+            if(touchedX < distance) {
+
+                Log.d("Blue right turn - ", blueGoRight.toString())
+                blueCar.animate().apply {
+                    duration = 500
+                    //rotationBy(30f)
+                    blueGoRight = if (blueGoRight) {
+                        translationXBy(294f)
+
+                        false
+                    } else {
+                        translationXBy(-294f)
+                        true
+                    }
+                }.start()
+
+            } else {
+
+                Log.d("Red right turn - ", redGoRight.toString())
+                redCar.animate().apply {
+                    duration = 500
+                    //rotationBy(-30f)
+                    redGoRight = if (redGoRight) {
+                        translationXBy(294f)
+                        false
+                    } else {
+                        translationXBy(-294f)
+                        true
+                    }
+                }.start()
+
+            }
+
+        }*/
+
+        screen.setOnTouchListener { _, event ->
+
+            if(event.action == MotionEvent.ACTION_DOWN) {
+
+                val currTime = event.eventTime
+                animationEnded = false
+
+                val touchedX: Float = event.x
+                Log.d("Abscissa", touchedX.toString())
+                Log.d("Time", event.eventTime.toString())
+                Log.d("Distance of line", distance.toString())
+
+                if (touchedX < distance) {
+
+                    if(currTime - bluePrevTime > 300) {
+                        bluePrevTime = currTime
+
+                        Log.d("Blue right turn - ", blueGoRight.toString())
+                        blueCar.animate().apply {
+                            duration = 300
+                            //rotationBy(30f)
+                            blueGoRight = if (blueGoRight) {
+                                translationXBy(294f)
+                                false
+                            } else {
+                                translationXBy(-294f)
+                                true
+                            }
+                        }.start()
+                    }
+                } else {
+
+                    if(currTime - redPrevTime > 300) {
+                        redPrevTime = currTime
+
+                        Log.d("Red right turn - ", redGoRight.toString())
+                        redCar.animate().apply {
+                            duration = 300
+                            //rotationBy(-30f)
+                            redGoRight = if (redGoRight) {
+                                translationXBy(294f)
+                                false
+                            } else {
+                                translationXBy(-294f)
+                                true
+                            }
+                        }.start()
+                    }
+                }
+                animationEnded = true
+
+            }
+            true
         }
 
     }
